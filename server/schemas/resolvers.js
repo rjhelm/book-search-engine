e5d03cde21c8e64672154ac2c3e232072a82778f
Ryan Helm <rjhelm@github.io>
2021-06-19 21:06:13 -0600

    added mutations for resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+const { sign } = require('jsonwebtoken');
 
 const resolvers = {
     Query: {
@@ -15,5 +16,24 @@ const resolvers = {
             throw new AuthenticationError('Not Logged In')
         },
     },
-    
+    Mutation: {
+        addUser: async (user, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token, user};
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+            if (!user) {
+                throw new AuthenticationError('Credentials Are Incorrect');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Credentials Are Incorrect');
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
+        
+    }
 }
