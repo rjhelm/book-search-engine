51be8c4130e67ca4b559b896a0d6dfe3f3c36e76
Ryan Helm <rjhelm@github.io>
2021-06-19 21:15:00 -0600

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -38,13 +38,25 @@ const resolvers = {
             if (context.user) {
                 const updatedUser = await User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { $addToSet: { savedBooks: args.input } },
                     { new: true }
                 );
                 return updatedUser;
             }
             throw new AuthenticationError('Must be logged in');
         },
-        
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Must be logged in')
+        }
     }
 }
+
+module.exports = resolvers;
