415e8117e06844c1bf07c3253aac705c1fd90230
Ryan Helm <rjhelm@github.io>
2021-06-19 21:01:11 -0600

    resolvers started

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        // user by username //
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate('books')
+                return userData;
+            }
+            throw new AuthenticationError('Not Logged In')
+        },
+    },
+    
+}
