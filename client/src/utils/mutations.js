0ca269e464bcb60c8b9b279f5c4801908838e3cc
Ryan Helm <rjhelm@github.io>
2021-06-19 20:42:31 -0600

    added gql mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,49 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password){ 
+        token user {
+            _id
+        }
+    }
+}`;
+
+export const ADD_USER = gql`mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+        user {
+            _id
+            username
+            email
+            bookcount
+            savedBooks {
+                authors
+                bookId
+                image
+                link
+                title
+                description
+            }
+        }
+        token
+    }
+}`;
+
+export const SAVE_BOOK = `gql mutation saveBook($bookId: ID!) {
+    saveBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            # _id
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}`;
+
+
