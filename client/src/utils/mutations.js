a2b9042d29e208d71902da7315d324ca3cd3d2c6
Ryan Helm <rjhelm@github.io>
2021-06-19 20:45:20 -0600

    added last mutation for removing a book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -46,4 +46,20 @@ export const SAVE_BOOK = `gql mutation saveBook($bookId: ID!) {
     }
 }`;
 
-
+export const REMOVE_BOOK = `gql mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            # _id
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}`;
