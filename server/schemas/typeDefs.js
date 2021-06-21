697e8ee5c5cdbb7cacff4ae9f03f4c75a51e84a6
Ryan Helm <rjhelm@github.io>
2021-06-20 19:04:55 -0600

    finished typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,5 +29,15 @@ input savedBook {
 type Query {
     me: User
 }
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): IUser
+    removeBook(bookId: ID!): User
+}
+type Auth {
+    token: ID!
+    user: User
+}`;
 
-`;
+module.exports = typeDefs;
