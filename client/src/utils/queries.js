2113820a944949b7cd1dcd2e5a0038a0013dd896
rjhelm <rjhelm@github.io>
2021-07-06 20:24:53 -0600

    deploy

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,25 @@
 import gql from 'graphql-tag';
 
-export const GET_ME = gql` {
+
+export const GET_ME = gql`
+{
     me {
-        _id
-        username
-        email
-        bookcount
-        savedBooks {
-            # _id
-            bookId
-            authors
-            image
-            link
-            title
-            description
-        }
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
     }
-}`;
+  }
+`;
+
+
+
